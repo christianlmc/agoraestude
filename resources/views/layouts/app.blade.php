5609b417e8853e9e8f0fdb4bd06bc629ccaf3f9b
Christian Luis <christianlmc1@gmail.com>
2018-08-02 21:17:36 -0300

    Arrumando página de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,59 +21,77 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+	<div id="app">
+		<nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+			<div class="container">
+				<div class="navbar-brand">
+					<a class="navbar-item" href="{{ url('/') }}">
+						<img src="{{ asset('favicon.ico') }}">
+						<h3 class="title is-4">{{ config('app.name', 'Laravel') }}</h3>
+					</a>
+					<a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
+				</div>
+				<div class="navbar-menu" id="navMenu">
+					<div class="navbar-start">
+						<!-- navbar items -->
+						<a class="navbar-item" href="https://agoraestude.com.br/">Site Principal</a>
+					</div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+					<div class="navbar-end">
+						@guest
+							<a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+						@else
+							<div class="navbar-item has-dropdown is-hoverable">
+								<a class="navbar-link">
+									{{ Auth::user()->nome }}
+								</a>
+								<div class="navbar-dropdown is-right">
+									<a class="navbar-item" href="{{ route('logout') }}"
+									onclick="event.preventDefault();
+									document.getElementById('logout-form').submit();">
+								{{ __('Sair') }}</a>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+								</div>
+							</div>
+						@endguest
+					</div>
+				</div>
+			</div>
+		</nav>
+        <section class="section">
             @yield('content')
-        </main>
+        </section>
     </div>
+    <script type="text/javascript">
+    	document.addEventListener('DOMContentLoaded', () => {
+
+		  // Get all "navbar-burger" elements
+		  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+		  // Check if there are any navbar burgers
+		  if ($navbarBurgers.length > 0) {
+
+		    // Add a click event on each of them
+		    $navbarBurgers.forEach( el => {
+		      el.addEventListener('click', () => {
+
+		        // Get the target from the "data-target" attribute
+		        const target = el.dataset.target;
+		        const $target = document.getElementById(target);
+
+		        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		        el.classList.toggle('is-active');
+		        $target.classList.toggle('is-active');
+
+		      });
+		    });
+		  }
+		});
+    </script>
 </body>
 </html>
