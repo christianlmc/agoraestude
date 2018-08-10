35ba6475301af6b6ae0cbe1efdac4b5b9ab3405b
Christian Luis <christianlmc1@gmail.com>
2018-08-10 15:38:55 -0300

    Adicionando botao voltar 'Painel Admin'

--- resources/views/admin/curso.blade.php
+++ resources/views/admin/curso.blade.php
@@ -8,7 +8,19 @@
 		<div class="column is-8 is-offset-2">
 			<div class="card">
 				<header class="card-header">
-		            <p class="card-header-title title is-4">Editar {{ $curso->nome }}</p>
+					<div class="columns card-header-title">
+						<div class="column">
+							<p class="title is-4">Editar {{ $curso->nome }}</p>
+						</div>
+						<div class="column">
+							<a class="button is-outlined is-primary is-pulled-right" href="{{ route('painel-admin') }}">
+								<span class="icon">
+									<i class="fas fa-arrow-circle-left"></i>
+								</span>
+								<span>Painel Admin</span>
+							</a>
+						</div>
+					</div>
 		        </header>
 		        <div class="card-content">
 		        	<editar-curso curso-id="{{ $curso->id }}" curso-nome="{{ $curso->nome }}"></editar-curso>
